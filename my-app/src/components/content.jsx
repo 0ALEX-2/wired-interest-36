34ddeeb41c564db23e0c79fc72cb3fd9f872fb18
MD Sakil Ahmed <sakilahmed965@gmail.com>
2022-11-15 23:20:09 +0530

    updated

--- my-app/src/components/content.jsx
+++ my-app/src/components/content.jsx
@@ -96,6 +96,89 @@ let KolkataData = [
   },
 ];
 
+let WhaleData=[
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g190475-d11480931-From_Tromso_all_inclusive_Whale_and_Seabird_Safari_by_Boat_to_Skervoy-Tromso_Troms.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/5b/47/a1/caption.jpg?w=300&h=300&s=1",
+    keSJi: "From Tromso all-inclusive Whale and Seabird Safari by Boat to Skervøy",
+    yyzcQ: 91,
+    biGQs: "from ₹15,846 per adult",
+    biGQt: ""
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g189970-d11457718-Classic_Whale_Watching_from_Reykjavik_The_Original-Reykjavik_Capital_Region.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/a9/38/78/caption.jpg?w=300&h=300&s=1",
+    keSJi: "Classic Whale Watching from Reykjavik | The Original",
+    yyzcQ: 914,
+    biGQs: "from ₹6,460 per adult",
+    biGQt: "Best Seller"
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g312663-d11456885-Boat_Based_Whale_Watching_from_Hermanus-Hermanus_Overstrand_Overberg_District_West.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/5c/26/40/caption.jpg?w=300&h=300&s=1",
+    keSJi: "Boat Based Whale Watching from Hermanus",
+    yyzcQ: 174,
+    biGQs: "from ₹5,340 per adult",
+    biGQt: ""
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g189963-d11467588-Whale_Safari_and_Puffins_RIB_Boat_Tour_from_Husavik-Husavik_Northeast_Region.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/85/b8/0a/caption.jpg?w=300&h=300&s=1",
+    keSJi: "Whale Safari and Puffins RIB Boat Tour from Húsavík",
+    yyzcQ: 219,
+    biGQs: "from ₹12,210 per adult",
+    biGQt: ""
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g60750-d12086300-San_Diego_Whale_Watching_Cruise-San_Diego_California.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/24/bb/7e/3d/caption.jpg?w=300&h=300&s=1",
+    keSJi: "San Diego Whale Watching Cruise",
+    yyzcQ: "1,235",
+    biGQs: "from ₹7,012 per adult",
+    biGQt: "Likely To Sell Out"
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g255106-d11454305-Auckland_Dolphin_and_Whale_Watching_Eco_Safari_Cruise-Auckland_Central_North_Islan.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/69/91/55/caption.jpg?w=300&h=300&s=1",
+    keSJi: "Auckland Dolphin and Whale Watching Eco-Safari Cruise",
+    yyzcQ: "1,153",
+    biGQs: "from ₹8,342 per adult",
+    biGQt: ""
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g32279-d11991692-Dana_Point_Whale_Watching_and_Dolphin_Watching_Eco_Safari-Dana_Point_California.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/22/b9/e6/3a/caption.jpg?w=300&h=300&s=1",
+    keSJi: "Dana Point Whale Watching and Dolphin Watching Eco-Safari",
+    yyzcQ: 367,
+    biGQs: "from ₹8,250 per adult",
+    biGQt: "Likely To Sell Out"
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g255374-d14054077-Whale_Watching_in_Kaikoura_by_Boat-Kaikoura_Canterbury_Region_South_Island.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/25/a8/3b/f8/caption.jpg?w=300&h=300&s=1",
+    keSJi: "Whale Watching in Kaikoura by Boat",
+    yyzcQ: "2,371",
+    biGQs: "from ₹8,556 per adult",
+    biGQt: "Likely To Sell Out"
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g1407334-d15885193-Whales_and_Dolphins_Watching_Luxury_Super_Grade_Cruiser-Mirissa_Southern_Province.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/48/5f/b1/img-20191213-wa0002-largejpg.jpg?w=300&h=300&s=1",
+    keSJi: "Whales and Dolphins Watching-Luxury Super Grade Cruiser",
+    yyzcQ: 720,
+    biGQs: "from ₹5,362 per adult",
+    biGQt: ""
+  },
+  {
+    //"BMQDV href": "https://www.tripadvisor.in/AttractionProductReview-g29220-d11486523-Whale_Watching_from_Lahaina_Harbor-Maui_Hawaii.html",
+    photo: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/e5/2d/ef/caption.jpg?w=300&h=300&s=1",
+    keSJi: "Whale Watching from Lahaina Harbor",
+    yyzcQ: 399,
+    biGQs: "from ₹6,819 per adult",
+    biGQt: ""
+  }
+]
+
 export const KolkataUpdate = () => {
   const settings = {
     dots: true,
@@ -136,15 +219,53 @@ export const KolkataUpdate = () => {
   };
   return (
     <div style={{width:"95%",margin:"auto"}}>
+      <div style={{textAlign:"left"}}>
       <h1>Ways to tour Kolkata (Calcutta)</h1>
       <p>Book these experiences for a close-up look at Kolkata (Calcutta).</p>
+      </div>
 
       <Slider {...settings}>
+     
         {
             KolkataData.map((ele)=>{
-               return <div>
+               return <div style={{paddingLeft:"8px"}}>
                     <div>
                         <img src={ele.image} alt="photo" width="270px" />
+                    </div>
+                    <div style={{textAlign:"left"}}>
+                      <h3>{ele.keSJi}</h3>
+                      <h4>{ele.biGQs}</h4>
+                    </div>
+                </div>
+            })
+        }
+      </Slider>
+
+      <div style={{margin:"auto",display:"flex",justifyContent:"space-between",height:"200px",marginTop:"50px",backgroundColor:"#fcc"}}>
+        <div style={{width:"55%",textAlign:"left",paddingLeft:"20px"}}>
+          <h2>Get out there</h2>
+          <p>Best of the Best tours,attractions you won't to miss.</p>
+          <button style={{fontSize:"20px",padding:"8px",borderRadius:"13px",backgroundColor:"black",color:"white"}}>See the list</button>
+        </div>
+        <div style={{width:"45%"}}>
+          <img src="https://dynamic-media-cdn.tripadvisor.com/media/photo-o/21/f7/1d/4d/caption.jpg?w=1000&h=-1&s=1" alt="photo" height="200px" width="100%"/>
+        </div>
+      </div>
+
+      <div style={{textAlign:"left"}}>
+      <h1>It’s whale-watching season</h1>
+      <p>Here's how to see nature’s largest wonders up-close.</p>
+      </div>
+      <Slider {...settings}>
+        {
+            WhaleData.map((ele)=>{
+               return <div >
+                    <div style={{}}>
+                        <img src={ele.photo} alt="photo" width="270px" />
+                    </div>
+                    <div style={{textAlign:"left"}}>
+                      <h3>{ele.keSJi}</h3>
+                      <h4>{ele.biGQs}</h4>
                     </div>
                 </div>
             })
